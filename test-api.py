a8ffcd44fc8b8ce13fe837caae5cfcb033183041
tarohnoji <tailangyede9@gmail.com>
2021-02-10 06:01:27 +0900

    modified

--- test-api.py
+++ test-api.py
@@ -2,15 +2,14 @@ import requests
 import json
 from pprint import pprint
 
-API_KEY = "1850147"
+API_KEY = "6bbba9547602f48e4f0af927d8116c05"
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather?units=metric&lang=ja"
-city = "Kobe,jp"
+city = "id,1850147"
 
 response = requests.get(BASE_URL + "&q={}&APPID={}".format(city, API_KEY))
 
-wheather_data = json.loads(response.text)
+weather_data = json.loads(response.text)
 
-pprint(wheather_data)
+weather_tokyo_now = weather_data["weather"][0]["description"]
 
-print("神戸の天気は" + weather_data[weather][description] + "です")
-
+print("東京の天気は{}です".format(weather_tokyo_now))
